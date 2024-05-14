9c6110bf874f7508a4240b5c20a0cae60db397c8
ravenstorm43 <201912065@office.induk.ac.kr>
2024-05-14 11:11:18 +0900

    update, delete 구현

--- src/main/java/com/sparta/memo/controller/MemoController.java
+++ src/main/java/com/sparta/memo/controller/MemoController.java
@@ -8,9 +8,10 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController
-@RequestMapping ("/api")
+@RequestMapping("/api")
 public class MemoController {
     private final Map<Long, Memo> memoList = new HashMap<>();
+
     @PostMapping("/memos")
     public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto) {
         // RequestDto -> Entity
@@ -26,11 +27,39 @@ public class MemoController {
         MemoResponseDto memoResponseDto = new MemoResponseDto(memo);
         return memoResponseDto;
     }
+
     @GetMapping("/memos")
     public List<MemoResponseDto> getMemos() {
         // Map to List
         List<MemoResponseDto> responseList = memoList.values().stream()
                 .map(MemoResponseDto::new).toList();
         return responseList;
+    }
+
+    @PutMapping("/memos/{id}")
+    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
+        // 해당 메모가 DB에 존재하는지 확인
+        if (memoList.containsKey(id)) {
+            Memo memo = memoList.get(id);
+
+            // memo 수정
+            memo.update(requestDto);
+            return memo.getId();
+        } else {
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+        }
+    }
+
+    @DeleteMapping("/memos/{id}")
+    public Long deleteMemo(@PathVariable Long id) {
+        if (memoList.containsKey(id)) {
+            Memo memo = memoList.get(id);
+
+            // memo 수정
+            memoList.remove(id);
+            return id;
+        } else {
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+        }
     }
 }
